8be86cdde5152e6d7eac4b8f41a118df1e00a84e
jiachenwang <17764519325@163.com>
2020-10-09 19:20:57 +0800

    change css

---
+++ precache-manifest.4167c205301a1ff1c991ac3e3d0931aa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fded223ec7219f8d8fca45f22c20c994",
+    "url": "https://zjuidg.org/index.html"
+  },
+  {
+    "revision": "06c1fa0af428f26a2d37",
+    "url": "https://zjuidg.org/static/css/main.ae9e76d2.chunk.css"
+  },
+  {
+    "revision": "20b728ff037a93152bf2",
+    "url": "https://zjuidg.org/static/js/2.45d531c9.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "https://zjuidg.org/static/js/2.45d531c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c1fa0af428f26a2d37",
+    "url": "https://zjuidg.org/static/js/main.bca3c62b.chunk.js"
+  },
+  {
+    "revision": "7fa82bad423e94a9b93f",
+    "url": "https://zjuidg.org/static/js/runtime-main.f7c17f6d.js"
+  },
+  {
+    "revision": "898cc0508ff9f711cd6ac34f0930d43e",
+    "url": "https://zjuidg.org/static/media/IDG-simplified.898cc050.gif"
+  },
+  {
+    "revision": "736cf5b08b01082a3645e14038868e20",
+    "url": "https://zjuidg.org/static/media/Krungthep.736cf5b0.ttf"
+  },
+  {
+    "revision": "4bb09e04c794eed387993385a7f8a3f8",
+    "url": "https://zjuidg.org/static/media/demo.4bb09e04.svg"
+  },
+  {
+    "revision": "34faa255a51b9e6230d628e84a9dacdd",
+    "url": "https://zjuidg.org/static/media/pdf.34faa255.svg"
+  },
+  {
+    "revision": "8074bd30a674876892fd55fdadeb493e",
+    "url": "https://zjuidg.org/static/media/video.8074bd30.svg"
+  },
+  {
+    "revision": "7ede8459ab46c029ae28fe432cde6b21",
+    "url": "https://zjuidg.org/static/media/zjuidg_grey.7ede8459.svg"
+  }
+]);
